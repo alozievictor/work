1f536abae1d87a7b5144c85182290a72a37b5d97
alozievictor <aloziev50@gmail.com>
2023-02-02 11:22:32 +0100

    working checked

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // const flash = require('')
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const app = express()
 
